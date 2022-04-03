eac045e8700dc6081e532906aec363dce712f896
Blake Smethers <smethersblake@gmail.com>
2022-04-03 15:43:43 -0500

    add answers and highscore

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,48 @@
-var questions = document.querySelector("#questions")
+var intro = document.querySelector("#intro")
 var startbtn = document.querySelector("#start-btn")
 var introText = document.querySelector("#intro")
 var timerText = document.querySelector("#timer")
+var answerCheck = document.querySelector(".answer-check")
+var s1 = document.querySelector('.s1')
+var s2 = document.querySelector('.s2')
+var s3 = document.querySelector('.s3')
+var s4 = document.querySelector('.s4')
+var s5 = document.querySelector('.s5')
+var highScores = []
 const selctions = [
     {
-        0: "answer 1",
-        1: "answer 2",
-        2: "answer 3",
-        3: "answer 4"
+        0: "A",
+        1: "B",
+        2: "C",
+        3: "D"
     },
     {
-        0: "answer 5",
-        1: "answer 6",
-        2: "answer 7",
-        3: "answer 8"
+        0: "A",
+        1: "B",
+        2: "C",
+        3: "D"
     },
     {
-        0: "answer 9",
-        1: "answer 10",
-        2: "answer 11",
-        3: "answer 12"
+        0: "A",
+        1: "B",
+        2: "C",
+        3: "D"
     },
     {
-        0: "answer 13",
-        1: "answer 14",
-        2: "answer 15",
-        3: "answer 16"
+        0: "A",
+        1: "B",
+        2: "C",
+        3: "D"
     },
     {
-        0: "answer 17",
-        1: "answer 18",
-        2: "answer 19",
-        3: "answer 20"
+        0: "A",
+        1: "B",
+        2: "C",
+        3: "D"
     }
+]
+const qText = [
+    "question one", "question two", "question three", "question four", "qustion five"
 ]
 
 var choices = []
@@ -40,10 +50,11 @@ var questionNum = 0;
 var timer = 100;
 var timerCountDown;
 var answerbtn = document.querySelector(".answer-btn");
-// startbtn.addEventListener("click", begin);
+
 startbtn.addEventListener("click", function() {
     // show the first set of questions
     populateAnswers();
+    startbtn.remove();
     timerCountDown = setInterval(function() {
         timer--;
         timerText.textContent = timer
@@ -51,16 +62,71 @@ startbtn.addEventListener("click", function() {
     }, 1000)
 })
 
-
+var questionText = function(){
+    null;
+}
 answerbtn.addEventListener("click", function(event) {
     populateAnswers(event);
-    // choices.push(event.target.textContent)
-    // console.log(choices)
 })
 var populateAnswers = function(event) {
     answerbtn.textContent = "";
+    solutions();
     if (event) {
         choices.push(event.target.textContent)
+        console.log(event.target)
+        answerCheck.textContent = ''
+        switch (questionNum) {
+            case 1:
+                if (event.target.textContent === 'C') {
+                    console.log("Yes")
+                    correctAnswer();
+                }
+                else {
+                    console.log('No')
+                    wrongAnswer();
+                }
+                break;
+            case 2:
+                if (event.target.textContent === 'B') {
+                    console.log("Yes")
+                    correctAnswer();
+                }
+                else {
+                    console.log('No')
+                    wrongAnswer();
+                }
+                break;
+            case 3:
+                if (event.target.textContent === 'D') {
+                    console.log("Yes")
+                    correctAnswer();
+                }
+                else {
+                    console.log('No')
+                    wrongAnswer();
+                }
+                break;
+            case 4:
+                if (event.target.textContent === 'C') {
+                    console.log("Yes")
+                    correctAnswer();
+                }
+                else {
+                    console.log('No')
+                    wrongAnswer();
+                }
+                break;
+            case 5:
+                if (event.target.textContent === 'D') {
+                    console.log("Yes")
+                    correctAnswer();
+                }
+                else {
+                    console.log('No')
+                    wrongAnswer();
+                }
+                break;
+        }
     }
     
     if (choices.length === 5) {
@@ -72,24 +138,107 @@ var populateAnswers = function(event) {
             var answertext = document.createElement("button");
             answertext.textContent = selctions[questionNum][i];
             answerbtn.append(answertext)
-    }
-
-
-        // get html on page
+        }
     }
     questionNum++;
-    // if (questionNum > choices.length) {
-    //     showAnswers();
-    //     console.log(choices)
-    //}
+}
+var highScoreSubmit = function player(name, score) {
+    this.name = name;
+    this.score = score;
+    console.log(name, score)
+    var player = [name, score]
+    if( score > highScores[1]) {
+        console.log('yes')
+        localStorage.setItem('player', JSON.stringify(player))
+    }
+    else {
+        console.log('no')
+    }
+    
 }
 var showAnswers = function() {
 
     clearInterval(timerCountDown)
-    for (let i = 0; i < choices.length; i++) {
-        var choicesData = document.createElement("h3");
-        choicesData.textContent = choices[i];
+    answerCheck.textContent = ''
+    introText.append('Your score is: ' + timer)
+    var submitText = document.createElement('input')
+    var submitbtn = document.createElement('button')
+    submitText.placeholder = 'Please enter name'
+    submitbtn.textContent = 'Submit'
+    answerCheck.appendChild(submitText)
+    answerCheck.append(submitbtn)
+    submitbtn.addEventListener('click', function() {
+        console.log('click')
+        highScoreSubmit(submitText.value, timer);
+        highScoreList();
         
-        answerbtn.append(choicesData)
+    })
+}
+var wrongAnswer = function() {
+    timer = timer - 10;
+    answerCheck.textContent = "Wrong Answer"
+}
+var correctAnswer = function() {
+
+    answerCheck.textContent = "Correct Answer"
+}
+var highScoreList = function() {
+    // intro.textContent = ''
+    intro.textContent = "High Score: " + highScores[0] + ': ' + highScores[1]
+    answerCheck.textContent = ''
+    var retryBtn = document.createElement('button')
+    retryBtn.textContent = "Retry"
+    answerCheck.append(retryBtn)
+}
+var solutions = function() {
+    solutionReset();
+    switch(questionNum) {
+        case 0:
+            introText.textContent = "Commonly used data types DO NOT include: "
+            s1.textContent = 'strings'
+            s2.textContent = 'booleans'
+            s3.textContent = "alerts"
+            s4.textContent = "numbers"
+            break;
+        case 1:
+            introText.textContent = "The condition in an if / else statment is enclosed with ______"
+            s1.textContent = 'quotes'
+            s2.textContent = 'curly brackets'
+            s3.textContent = 'parenthesis'
+            s4.textContent = 'square brackets'
+            break;
+        case 2:
+            introText.textContent = "A very useful tool used during development and debuggin for printing content to the bebugger is:"
+            s1.textContent = 'JavaScript'
+            s2.textContent = "terminal/bash"
+            s3.textContent = 'for loops'
+            s4.textContent = 'console.log'
+            break;
+        case 3:
+            introText.textContent = "String values must be enclosed within ____ when being assigned to variables."
+            s1.textContent = 'commas'
+            s2.textContent = 'curly brackets'
+            s3.textContent = 'quotes'
+            s4.textContent = 'parenthesis'
+            break;
+        case 4:
+            introText.textContent = 'Arrays in JavaScript can be used to store _____.'
+            s1.textContent = 'numbers ans strings'
+            s2.textContent = 'other arrays'
+            s3.textContent = 'booleans'
+            s4.textContent = 'all of the above'
     }
 }
+var solutionReset = function() {
+    introText.textContent = ''
+    s1.textContent = ''
+    s2.textContent = ''
+    s3.textContent = ''
+    s4.textContent = ''
+}
+var loadScores = function() {
+    var scores = JSON.parse(localStorage.getItem('player'))
+    highScores = scores
+    console.log(highScores[1])
+}
+loadScores();
